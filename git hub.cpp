8b4fde8e9964a76f3d02c8c2bf3756d33c9b9670
okmrng <115197394+okmrng@users.noreply.github.com>
2022-11-18 10:10:23 +0900

    リモートリポジトリでコンフリクト変更

--- git hub.cpp
+++ git hub.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello Github World!\n";
 
-    std::cout << "Github Clone changed\n";
+    std::cout << "Github clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
